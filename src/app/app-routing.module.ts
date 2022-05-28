82e11b486fffcf39b8efad5f1ba1ddba5ce61d3b
zakaria <zakariajabilou@gmail.com>
2022-05-28 19:21:39 +0000

    commit 3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CutomersComponent} from "./cutomers/cutomers.component";
+import {AccountsComponent} from "./accounts/accounts.component";
+import {NewCustomerComponent} from "./new-customer/new-customer.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"customers",component:CutomersComponent},
+  {path:"accounts",component:AccountsComponent},
+  {path:"new-customer",component:NewCustomerComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
